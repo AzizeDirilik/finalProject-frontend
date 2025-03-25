160df2ec7e331fb0c93bfdd2d694ec42eacb4d7b
AzizeDirilik <azizedirilik6@gmail.com>
2025-03-25 16:35:58 +0300

    Category, Navi, and Product components added, models updated

---
+++ src/app/components/navi/navi.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navi',
+  standalone: false,
+  templateUrl: './navi.component.html',
+  styleUrl: './navi.component.css'
+})
+export class NaviComponent {
+
+}
